722500b394da0fc4d591e0f7f39e1c0bca286478
SajinKowserSK <sajinkowser@gmail.com>
2020-05-29 01:20:59 -0400

    binary search problems

---
+++ practice.py
@@ -0,0 +1,22 @@
+def searchInsert(arr, target):
+    start = 0
+    end = len(arr)-1
+
+    while (start <= end):
+
+        mid = int(start+(end-start)/2)
+
+        if arr[mid] <= target:
+            if arr[mid] == target or mid == len(arr)-1:
+                return mid
+
+            start = mid + 1
+
+        else:
+            if mid == 0 or arr[mid-1] == target:
+                return mid
+
+            end = mid - 1
+
+sajin = [1,2,3,4,5,6]
+print(searchInsert(sajin, 2))
