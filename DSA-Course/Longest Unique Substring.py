0cad8ebb5c2129caf77983a7be3f1c788a86d94b
SajinKowserSK <sajinkowser@gmail.com>
2020-07-01 17:18:36 -0400

    Medium, O(n) time, O(n) space

---
+++ DSA-Course/Longest Unique Substring.py
@@ -0,0 +1,67 @@
+from helpers import *
+
+def longest(arr):
+    start = 0
+    end = 0
+    finStart = 0
+    finEnd = 0
+    myMap = {}
+
+    while end < len(arr):
+        if start > end:
+            start = end
+            end = end + 1
+
+        else:
+            nextItem = arr[end]
+
+            if nextItem not in myMap:
+                myMap[nextItem] = end
+                end = end + 1
+
+            else:
+                # recall that we updated end to be the next item
+                # (so up to end-1 is unique)
+
+                if end-1-start+1 > finEnd - finStart + 1:
+                    finStart = start
+                    finEnd = end-1
+
+                del myMap[arr[start]]
+                start = start + 1
+
+        # check for in case substring is start to the end of the array 
+        if end - 1 - start + 1 > finEnd - finStart + 1:
+            finStart = start
+            finEnd = end - 1
+
+
+    return pair(finStart, finEnd)
+
+
+def substring2(arr):
+    start = 0
+    end = 0
+    longest = 1
+    result = [0,0]
+    myMap = {}
+
+    while end < len(arr)-1:
+        end += 1
+        ch = arr[end]
+
+        if ch in myMap and myMap[ch] >= start:
+            start = myMap[ch] + 1
+
+        myMap[ch] = end
+
+        if end - start + 1 > longest:
+            longest = end - start + 1
+            result[0] = start
+            result[1] = end
+
+    return result
+
+test = chArr("sajapgjyzt")
+print(longest(test))
+print(substring2(test))
---
+++ DSA-Course/helpers.py
@@ -0,0 +1,14 @@
+# returns pair
+def pair(x, y):
+    return ("("+str(x)+", "+str(y)+")")
+
+# takes str and puts to array of chars format
+def chArr(str):
+    charArray = []
+    for x in range (0, len(str)):
+        charArray.append(str[x])
+
+    return charArray
+
+
+
