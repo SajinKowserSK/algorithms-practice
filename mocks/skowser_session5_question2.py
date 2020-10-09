efa3aa25787d81d5bdd4f1d7b588dcd035936aca
SajinKowserSK <sajinkowser@gmail.com>
2020-10-09 17:45:54 -0400

    kadanes algo lel

---
+++ mocks/skowser_session5_question2.py
@@ -0,0 +1,23 @@
+def maxSubArray(nums):
+    if nums is None or len(nums) == 0:
+        return None
+
+    max_sum = nums[0]
+    contig = nums[0]
+
+    for x in range(1, len(nums)):
+
+        curr = nums[x]  # 4
+        both = contig + curr  # 3
+
+        if both > curr:
+            contig = both
+
+        else:
+            contig = curr  # 4
+
+        max_sum = max(max_sum, contig)
+
+    return max_sum
+
+print(maxSubArray([-1,4,5,-7,3]))
