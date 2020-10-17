da7340127f54f317c48ccf71627ab7294035c03b
SajinKowserSK <sajinkowser@gmail.com>
2020-10-16 23:19:57 -0400

    must optimize but helpful helper

---
+++ leetcode old&new/Find KxK Sub Matrix.py
@@ -0,0 +1,37 @@
+def kxk_matrix(matrix, size):
+    numbers = matrix
+    submatrices = []
+
+    side = 0
+    down = 0
+
+    divisons = int(len(numbers[0]) / size)
+
+    while side < len(numbers[0]):
+        sub_matrix = []
+
+        for y in range(0, divisons):
+
+            for x in range(0, len(numbers)):
+                curr_arr = numbers[x]
+                slice = curr_arr[side:side+size]
+                for num in slice:
+                    sub_matrix.append(num)
+
+                print(curr_arr, slice, sub_matrix)
+
+
+                if len(sub_matrix) == size * size:
+                    submatrices.append(sub_matrix)
+                    sub_matrix = []
+
+            side = side + size
+
+    return submatrices
+
+print(kxk_matrix(
+[[1,2,3,4,5,6,7,8,9],
+ [10,11,12,13,14,15,16,17,18],
+ [19,20,21,22,23,24,25,26,27]], 3))
+
+
