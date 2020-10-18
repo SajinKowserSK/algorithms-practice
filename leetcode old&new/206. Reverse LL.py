4572f0d9027ea1159ae914332cb78f9b1707fc21
SajinKowserSK <sajinkowser@gmail.com>
2020-10-17 22:06:24 -0400

    added recursive version

---
+++ leetcode old&new/206. Reverse LL.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        curr = head
+
+        while curr is not None:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        return prev
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        curr = head
+
+        return self.reverse(prev, curr)
+
+    def reverse(self, prev, curr):
+        if curr is None:
+            return prev
+
+        next_node = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next_node
+        return self.reverse(prev, curr)
+
+
