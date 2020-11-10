1e2bcfd8b20fed6b656ca381b7168ff94bc50ab3
SajinKowserSK <sajinkowser@gmail.com>
2020-11-09 21:54:23 -0500

    bare brute

---
+++ leetcode old&new/56. Merge Intervals.py
@@ -0,0 +1,28 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        if len(intervals) == 0:
+            return []
+
+        intervals = sorted(intervals)
+        x = 0
+        while x < len(intervals) - 1:
+
+            curr_pair = intervals[x]
+            next_pair = intervals[x + 1]
+
+            if next_pair[0] >= curr_pair[0] and next_pair[0] <= curr_pair[1]:
+                new_first = curr_pair[0]
+                new_end = max(curr_pair[1], next_pair[1])
+
+                intervals.pop(x)
+                intervals.pop(x)
+                intervals.insert(x, [new_first, new_end])
+
+                x = 0
+                continue
+
+            x += 1
+
+        return intervals
+
