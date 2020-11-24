405fd8e750d6b05e0b33d8e1711743c440c975f4
SajinKowserSK <sajinkowser@gmail.com>
2020-11-24 18:56:50 -0500

    optimized

--- real/Longest Palandromic Substring.py
+++ real/Longest Palandromic Substring.py
@@ -1,6 +1,33 @@
 # O(n^2) optimized soln
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
 
-# O(n^3) soln
+        start = 0
+        end = 0
+
+        for x in range(0, len(s)):
+            len1 = self.helper(s, x, x)
+            len2 = self.helper(s, x, x + 1)
+            maxlen = max(len1, len2)
+
+            if maxlen > (end - start):
+                start = x - int(((maxlen - 1) / 2))
+                end = x + int(maxlen / 2)
+
+        return s[start:end + 1]
+
+    def helper(self, s, left, right):
+
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left = left - 1
+            right = right + 1
+
+        return right - left - 1
+
+
+
+
+    # O(n^3) soln
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
