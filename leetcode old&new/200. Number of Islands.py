6f8549d25e25c23132e6f90165b7294282b48e50
SajinKowserSK <sajinkowser@gmail.com>
2020-11-25 17:16:21 -0500

    turns out I did this question before

--- leetcode old&new/200. Number of Islands.py
+++ leetcode old&new/200. Number of Islands.py
@@ -27,3 +27,27 @@ class Solution:
             return 1
 
 
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        count = 0
+
+        for x in range(0, len(grid)):
+            for y in range(0, len(grid[0])):
+                if grid[x][y] == "1":
+                    count += 1
+                    self.dfs(grid, x, y)
+
+        return count
+
+    def dfs(self, grid, x, y):
+        if x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0]):
+            if grid[x][y] == "1":
+                grid[x][y] = "0"
+                self.dfs(grid, x + 1, y)
+                self.dfs(grid, x - 1, y)
+                self.dfs(grid, x, y + 1)
+                self.dfs(grid, x, y - 1)
+
+
+
