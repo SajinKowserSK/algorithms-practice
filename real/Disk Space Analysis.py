01a203012574a3f10fadcde38a019726ed16ef9e
SajinKowserSK <sajinkowser@gmail.com>
2020-11-28 14:42:39 -0500

    added optimal solution with dequeue

--- real/Disk Space Analysis.py
+++ real/Disk Space Analysis.py
@@ -16,11 +16,48 @@ def analysis(numComputer, hardDiskSpace, segmentLength):
 
     return max_val
 
+# optimized soln -> O(n) TC and O(n) SC
+def analysis2(numComputer, hardDiskSpace, segmentLength):
+    max_minima = float("-inf")
+    i = 0
+    dq = [] # stores indices, with the first being the most min val and the last being the max val
+    # dq holds items in ascending order, dq[0] will give us min for the subarray, and dq[-1] would give us max
+    while i < numComputer:
+        curr_elem = hardDiskSpace[i]
+        start_window = i - segmentLength + 1
 
-hardDiskSpace = [8,2,4]
+        if dq and dq[0] < start_window:
+            dq.pop(0)
+
+        # now that we have a valid dq range we need to add the current elem to update the dq's mins and maxs
+        # because we want min at the start, max at the bottom, we pop all elems right to left so long as
+        # curr_elem < dq[-1]
+
+        while dq and curr_elem < hardDiskSpace[dq[-1]]:
+            dq.pop(-1)
+
+        dq.append(i)
+
+        # now we can access the window's minima by doing dq[0]
+        # but we only want to do this if we've processed atleast segmentLen elems
+
+        if i >= segmentLength -1:
+            curr_window_min = hardDiskSpace[dq[0]]
+            max_minima = max(max_minima, curr_window_min)
+
+        i += 1
+
+    return max_minima
+
+
+
+
+
+hardDiskSpace = [8,2,4,3]
 numComputer = len(hardDiskSpace)
 segLen = 2
 
 print(analysis(numComputer, hardDiskSpace, segLen))
+print(analysis2(numComputer, hardDiskSpace, segLen))
 
 
