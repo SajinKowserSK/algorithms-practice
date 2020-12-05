6f8fb8c80dcc3b858698eef606e2b95606a5d28f
sajinkowser <sajinkowser@gmail.com>
2020-12-04 23:23:56 -0500

    added comments + review

--- real/Cut Off Rank.py
+++ real/Cut Off Rank.py
@@ -1,20 +1,23 @@
+
+# TC -> O(nlogn) SC -> O(n)
 def cutOff(scores, cutOff):
 
-    scores.sort(reverse=True)
+    scores.sort(reverse=True) # high scores
     scoreMap = {}
     counter = 0
 
     for x in range(0, len(scores)):
         curr = scores[x]
 
+        # updates rank according to their index position
         if curr not in scoreMap:
             rank = x + 1
             scoreMap[curr] = rank
-
+        # if it's already in it, get the first rank occurence
         else:
             rank = scoreMap[curr]
 
-
+        # increment if rank is below or equal to cutoff
         if rank <= cutOff:
             counter += 1
 
