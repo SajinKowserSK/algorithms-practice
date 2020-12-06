7439206d428175819081308b5c64709fcc0c6a10
sajinkowser <sajinkowser@gmail.com>
2020-12-06 17:52:32 -0500

    might have to redo TC and SC

--- real/Fill The Truck.py
+++ real/Fill The Truck.py
@@ -1,5 +1,12 @@
+# TC -> O(n * m) for first part, O(nmlognm) for heap part
+# SC -> O(n)
+
+
 import heapq
 def fillTruck(num, boxes, unitSize, unitsPerBox, truckSize):
+
+    # first create list where "box" occurs unitsPerBox times
+    # ie boxes = [1,2,3] and unitsPerBox = [3,2,1] -> product 0 will be 1 box and 3 times so [1,1,1], insert at front
     packed_products = []
     for x in range(0, len(boxes)):
         curr_box = boxes[x]
@@ -10,12 +17,15 @@ def fillTruck(num, boxes, unitSize, unitsPerBox, truckSize):
         entry = [int(char) for char in entry]
         packed_products += entry
 
+
+    # multiply them by -1 so we can get a max heap
     packed_products = [num * -1 for num in packed_products]
     heapq.heapify(packed_products)
 
     size = 0
     i = 0
 
+    # pop it amount of times = truck size
     while packed_products and i < truckSize:
         popped = heapq.heappop(packed_products)
         popped = abs(popped)
