1e71c47280443915a6dd761bf9e86133492d71b7
Rafit Jamil <rafit.jamil@gmail.com>
2020-12-13 17:51:42 -0500

    Update Cut Off Rank.py

--- real/Cut Off Rank.py
+++ real/Cut Off Rank.py
@@ -23,6 +23,29 @@ def cutOff(scores, cutOff):
 
     return counter
 
+# If 0 <= scores[i] <= 100
+# TC -> O(N)
+def cutOff(scores, cutOff):
+    if cutOff <= 0:
+        return 0
+    
+    freq = [0 for i in range(100+1)]
+    for score in scores:
+        freq[score] += 1
+       
+   ans = 0
+   currRank = 1
+   for i in range(100,-1,-1):
+        if freq[i] != 0:
+            ans += freq[i]
+            currRank += freq[i]
+            if currRank > cutOff;
+                break
+   return ans
+       
+       
+
 test = [100,25,50,50]
 test2 = [2,2,3,4,5]
 print(cutOff(test, 2))
+
