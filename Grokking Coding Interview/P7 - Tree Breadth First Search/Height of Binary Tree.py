b44c9a4b56c5133c7cde3d40cbce87c4059dc92a
sajinkowser <sajinkowser@gmail.com>
2021-08-09 23:46:55 -0400

    iterative way to find height of BT, O(N) TC and SC

---
+++ Grokking Coding Interview/P7 - Tree Breadth First Search/Height of Binary Tree.py
@@ -0,0 +1,45 @@
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def find_minimum_depth(root):
+  q = []
+  master = []
+
+  q.append(root)
+
+  while q:
+    lvlSize = len(q)
+    currLvl = []
+
+    for node in range(lvlSize):
+      popped = q.pop(0)
+
+      if popped.left:
+        q.append(popped.left)
+
+      if popped.right:
+        q.append(popped.right)
+
+      currLvl.append(popped.val)
+
+    master.append(currLvl)
+
+  return len(master)
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
+  root.left.left = TreeNode(9)
+  root.right.left.left = TreeNode(11)
+  print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
+
+
+main()
