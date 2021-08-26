4ab81fb5f15a7a8214c52d89ec156751f5c2358b
sajinkowser <sajinkowser@gmail.com>
2021-08-26 17:35:17 -0400

    another solution to older question

--- Grokking Coding Interview/P11 - Modified Binary Search/Bitonic Array.py
+++ Grokking Coding Interview/P11 - Modified Binary Search/Bitonic Array.py
@@ -1,27 +1,46 @@
-def find_max_in_bitonic_array(arr):
-    start, end = 0, len(arr) - 1
+# def find_max_in_bitonic_array(arr):
+#     start, end = 0, len(arr) - 1
+#
+#     while start <= end:
+#         mid = start + (end - start) / 2
+#         mid = int(mid)
+#
+#         if mid > 0 and mid < len(arr) - 1:
+#             if arr[mid + 1] < arr[mid] > arr[mid - 1]:
+#                 return arr[mid]
+#
+#             if arr[mid + 1] > arr[mid]:
+#                 start = mid + 1
+#
+#             else:
+#                 end = mid - 1
+#
+#         if mid == 0 and arr[mid] > arr[mid + 1]:
+#             return arr[mid]
+#
+#         if mid == len(arr) - 1 and arr[mid] > arr[mid - 1]:
+#             return arr[mid]
+#
+#     return -1
 
-    while start <= end:
-        mid = start + (end - start) / 2
+def find_max_in_bitonic_array(arr):
+    start, end = 0, len(arr)-1
+
+    while start < end:
+        mid = start + (end-start)/2
         mid = int(mid)
 
-        if mid > 0 and mid < len(arr) - 1:
-            if arr[mid + 1] < arr[mid] > arr[mid - 1]:
-                return arr[mid]
+        if arr[mid+1] > arr[mid]:
+            # we are still in the lesser half so make start = mid + 1
+            start = mid + 1
 
-            if arr[mid + 1] > arr[mid]:
-                start = mid + 1
+        else:
+            # arr mid is greater so its left could also be greater
+            end = mid
 
-            else:
-                end = mid - 1
+    # both start and end will point to the max now because the condition to break is they equal each other
+    return arr[start]
 
-        if mid == 0 and arr[mid] > arr[mid + 1]:
-            return arr[mid]
-
-        if mid == len(arr) - 1 and arr[mid] > arr[mid - 1]:
-            return arr[mid]
-
-    return -1
 
 
 def main():
