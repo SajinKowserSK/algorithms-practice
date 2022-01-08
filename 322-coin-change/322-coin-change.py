aefbad6e51028077225afe323787de66447df1d8
Sajin Kowser <52130952+SajinKowserSK@users.noreply.github.com>
2022-01-07 23:15:39 -0500

    Time: 1516 ms (50.31%), Space: 14.4 MB (91.86%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -3,26 +3,15 @@ class Solution:
         if amount == 0:
             return 0 
         
-        # fill out a dp table with max 
-        # amount of coins to create an amount from 0 -> amount (inclusive)
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0 #base case 
         
+        dp = [amount + 1] * (amount + 1) # dp list from 0 to amount 
+        dp[0] = 0 #to make 0 amt, you need 0 coins 
         
-        # fill up dp table, start at 1 and go to amount inclusive 
-        for a in range(1, amount + 1):
+        for a in range(1, amount+1):
             for c in coins:
-                if (a-c) >= 0: # the coin is not too big and can sum to amt            
+                if a >= c and (a-c) >= 0:
+                    dp[a] = min(dp[a], dp[a-c] + 1)
                     
-                    # recurrence relation 
-                    dp[a] = min(dp[a], 1 + dp[a-c])
-                  
-        # amount makeable with given coins and not set to default amount 
-        if dp[-1] != amount + 1:
-            return dp[-1]
-        
-        return -1 
-                    
-                    
+        return -1 if dp[-1] == amount + 1 else dp[-1]
                     
                     
