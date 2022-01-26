f9f13f02440a0d61245ea59b01116193bc65949c
skowser <skowser@uwo.ca>
2022-01-26 11:57:46 -0500

    netflix

--- test.py
+++ test.py
@@ -1,26 +1,20 @@
-class Solution:
-    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stk = []
-        for ast in asteroids:
-            if len(stk) == 0 or ast > 0:
-                stk.append(ast)
 
-            else:  # for collision, peak has to be going right and curr has to be going left
-                while stk and stk[-1] > 0 and ast < 0:
-                    print(stk[-1], ast)
-                    collision = stk[-1] + ast
-                    if collision > 0:  # peak is greater, keep just peak
-                        continue
+def find(memory, size):
+    s = memory.index(0)
+    j = s
 
-                    elif collision == 0:
-                        stk.pop(-1)
+    while j < len(memory)-1:
 
-                    else:  # incoming is greater
-                        stk.pop(-1)
-                        stk.append(collision)
-        return stk
+        if (j-s+1) == size:
+            break
 
+        j += 1
+        if memory[j] == 1:
+            s = memory.index(0, j)
+            j = s
 
+    print((s,j))
 
+memory = [0,1,0,0,0,1,0]
+find(memory, 3)
 
-    print(self.asteroidCollision())
