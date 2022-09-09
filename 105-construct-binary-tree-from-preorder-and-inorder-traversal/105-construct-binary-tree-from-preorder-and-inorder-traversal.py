1885a1ef17c518620793c25a444944fd3d6750f6
Sajin Kowser <52130952+SajinKowserSK@users.noreply.github.com>
2022-09-08 17:16:58 -0700

    Time: 461 ms (10.41%), Space: 88.6 MB (36.51%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -9,9 +9,14 @@ class Solution:
         if len(preorder) == 0 or len(inorder) == 0:
             return None 
         
+        # pre order processes root first, so start of the arr will be the root 
+        # in order does left -> root -> right so the inorder mid will be the root 
         root = TreeNode(preorder[0])
         mid = inorder.index(preorder[0])
         
+        # we know where the root is in order
+        # for the left subtree we will need 1:mid (inclusive) nodes because that's how many are left of the root 
+        # for the right subtree we will need mid+1 to end nodes because thats how many are right to the root
         root.left = self.buildTree(preorder[1:mid+1], inorder[0:mid])
         root.right= self.buildTree(preorder[mid+1::], inorder[mid+1::])
         
