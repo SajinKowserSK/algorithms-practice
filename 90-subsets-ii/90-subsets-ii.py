175b60e5ba9fef4d4ab9ee85f2b3ab4823922893
Sajin Kowser <52130952+SajinKowserSK@users.noreply.github.com>
2022-09-14 17:52:48 -0700

    Time: 78 ms (17.82%), Space: 14.2 MB (49.25%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,28 +1,31 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         master = []
+        
+        # have to sort, TC here is not an issue as entire algo is O(N x 2**N)
         nums.sort()
-        self.dfsHelper(nums, master, [], "", 0, set())
+        self.dfsHelper(nums, master, [], 0)
         return master 
         
-    def dfsHelper(self, nums, master, curr, strRep, i, seen):
-        if strRep in seen:
-            return
-        
-        if i >= len(nums):
-            master.append(curr.copy())
-            seen.add(strRep)
+    def dfsHelper(self, nums, master, subset, i):
+        if i == len(nums):
+            master.append(subset.copy())
             return 
         
-        else:
-            # left will include the element 
-            curr.append(nums[i])
-            self.dfsHelper(nums, master, curr, strRep + str(nums[i]), i+1, seen)
+            
+        # case where we include nums[i]
+        subset.append(nums[i])
+        self.dfsHelper(nums, master, subset, i+1)
 
+        # case where we don't include nums[i]
+        subset.pop()
+        while i < len(nums)-1 and nums[i] == nums[i+1]: # iterate to non dup elem
+            i += 1 
 
-            # right will not 
-            curr.pop()
-            self.dfsHelper(nums, master, curr, strRep, i+1, seen)
+        self.dfsHelper(nums, master, subset, i+1)
+            
+            
+       
         
         
         
